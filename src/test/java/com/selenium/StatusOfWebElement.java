191fc761c2f152bece06d8bd38ed5c87dc5e1082
unknown <vishalkumarsinghv60@gmail.com>
2025-12-18 15:23:45 +0530

    check status of web element

--- src/test/java/com/selenium/StatusOfWebElement.java
+++ src/test/java/com/selenium/StatusOfWebElement.java
@@ -10,7 +10,16 @@ public class StatusOfWebElement {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://demowebshop.tricentis.com/");
 		WebElement searchStore = driver.findElement(By.xpath("//input[@value='Search store']"));
-		System.out.println(searchStore.isDisplayed());
+		System.out.println(searchStore.isDisplayed()); // To check element is display or not
+		System.out.println(searchStore.isEnabled()); // to check is element enabled to enter data
+		// is selected()
+		WebElement checkRadioButton1 = driver.findElement(By.xpath("//input[@id='pollanswers-1']"));
+		System.out.println(checkRadioButton1.isSelected());
+		WebElement checkRadiobutton2 = driver.findElement(By.xpath("//input[@id='pollanswers-2']"));
+		System.out.println("Before selecting : " + checkRadiobutton2.isSelected());
+		checkRadiobutton2.click();
+		System.out.println("After selecting : " + checkRadiobutton2.isSelected());
+
 	}
 
 }
