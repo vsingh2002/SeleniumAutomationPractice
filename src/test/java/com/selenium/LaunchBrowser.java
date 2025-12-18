40486b1b1e20a683a92817bf91055576eb16338c
unknown <vishalkumarsinghv60@gmail.com>
2025-12-18 12:39:12 +0530

    make changes in the code

--- src/test/java/com/selenium/LaunchBrowser.java
+++ src/test/java/com/selenium/LaunchBrowser.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LaunchBrowser {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://demowebshop.tricentis.com/");
+		//driver.get("https://demowebshop.tricentis.com/");
 	}
 
 }
