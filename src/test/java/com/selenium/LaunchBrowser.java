c64e47bab5b352f597ca40bb81a4e96fc9c7dd61
unknown <vishalkumarsinghv60@gmail.com>
2025-12-18 11:44:50 +0530

    launch browser

---
+++ src/test/java/com/selenium/LaunchBrowser.java
@@ -0,0 +1,12 @@
+package com.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/");
+	}
+
+}
